942dca53e1978cd77d92984ad649db48f5435a59
Andrei Barbu <andrei.barbu@uipath.com>
2019-10-18 09:47:40 +1100

    test

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 JSON_STRING=$( jq -n \
-              --arg client_id "$packerClientId \
-              --arg subscription_id "$subscriptionId \
-              --arg build_resource_group_name "$resourceGroupName" \
-              --arg managed_image_resource_group_name "$resourceGroupName" \
-              --arg client_secret "$packerClientSecret" \
+              --arg client_id "$PACKERCLIENTID \
+              --arg subscription_id "$SUBSCRIPTIONID \
+              --arg build_resource_group_name "$RESOURCEGROUPNAME" \
+              --arg managed_image_resource_group_name "$RESOURCEGROUPNAME" \
+              --arg client_secret "$PACKERCLIENTSECRET" \
               '{client_id: $client_id, subscription_id: $subscription_id, build_resource_group_name: $build_resource_group_name}' )
 
 echo $JSON_STRING
