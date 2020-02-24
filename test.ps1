15f68b4a844ddccc8597569fc605b7fde532fe06
Andrei Barbu <48005330+AndreiBarbuOz@users.noreply.github.com>
2020-02-25 10:51:50 +1100

    clean

--- test.ps1
+++
@@ -1,5 +0,0 @@
-
-Write-Host "test"
-Write-Host $($repositoryName)
-
-Write-Host " $($ENV:repositoryName) "
--- test.sh
+++
@@ -1,9 +0,0 @@
-JSON_STRING=$( jq -n \
-              --arg client_id "$PACKERCLIENTID" \
-              --arg subscription_id "$SUBSCRIPTIONID" \
-              --arg build_resource_group_name "$RESOURCEGROUPNAME" \
-              --arg managed_image_resource_group_name "$RESOURCEGROUPNAME" \
-              --arg client_secret "$PACKERCLIENTSECRET" \
-              '{client_id: $client_id, subscription_id: $subscription_id, build_resource_group_name: $build_resource_group_name}' )
-
-echo $JSON_STRING
